87a1803ee5aed054dce8c8f6157a745ed2f67204
Usenmfon <usenmfonuko@gmail.com>
2023-08-03 16:23:18 +0100

    fix: corrected the enums value

--- app/Enums/ProductStatusEnum.php
+++ app/Enums/ProductStatusEnum.php
@@ -4,6 +4,6 @@ namespace App\Enums;
 
 enum ProductStatusEnum:string {
     case Pending = 'pending';
-    case Aprroved = 'Aprroved';
-    case Rejected = 'Rejected';
+    case Approved = 'approved';
+    case Rejected = 'rejected';
 }
