663b2b31d14a37ffc054b12d5bab53ae5f61f698
Usenmfon <usenmfonuko@gmail.com>
2023-08-09 01:49:12 +0100

    fix: added alpineJs to set-timeout for success messages

--- resources/views/layouts/app-master.blade.php
+++ resources/views/layouts/app-master.blade.php
@@ -44,6 +44,7 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="{!! url('assets/bootstrap/js/bootstrap.bundle.min.js') !!}"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
     @section("scripts")
 
