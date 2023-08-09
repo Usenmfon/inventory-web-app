cd59996ce5ab2e227b8afb995045c68de879f749
Usenmfon <usenmfonuko@gmail.com>
2023-08-09 01:58:31 +0100

    chore: all users can now see the approved products on homepage

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -11,22 +11,22 @@
         @guest
             <h1>Inventory Management App</h1>
             <p class="lead">You are viewing the home page. Please login to view the restricted data.</p>
-            <div class="row">
-                @foreach ($products as $key => $product)
-                    <div class="col-sm-6 mb-4">
-                        <div class="card h-100">
-                            <div class="card-body">
-                                <h5 class="card-title">Product Name: {{ $product->name }}</h5>
-                                <p class="card-text">{{ $product->description }}</p>
-                                <a href="#" class="btn btn-primary">{{ $product->price }}</a>
-                            </div>
+        @endguest
+        <div class="row">
+            @foreach ($products as $key => $product)
+                <div class="col-sm-6 mb-4">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h5 class="card-title">Product Name: {{ $product->name }}</h5>
+                            <p class="card-text">{{ $product->description }}</p>
+                            <a href="#" class="btn btn-primary">{{ $product->price }}</a>
                         </div>
                     </div>
-                @endforeach
-            </div>
-            <div class="d-flex mt-4">
-                {!! $products->links() !!}
-            </div>
-        @endguest
+                </div>
+            @endforeach
+        </div>
+        <div class="d-flex mt-4">
+            {!! $products->links() !!}
+        </div>
     </div>
 @endsection
